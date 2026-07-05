66c370b096bf9ed9a81588c9051441559e9ce1ad
yukyoungs <syk9848@gmail.com>
2026-07-05 17:03:57 +0900

    Solve: Lv1 정수 제곱근 판별

--- Programmers/Lv1_정수_제곱근_판별/Solution.java
+++ Programmers/Lv1_정수_제곱근_판별/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public long solution(long n) {
         long answer = 0;
+
+        double result = Math.sqrt(n);
+
+        if ((long) result * (long) result == n) {
+            long result2 = (long) result + 1;
+            answer = result2 * result2;
+        } else {
+            answer = -1;
+        }
+
         return answer;
     }
 }
