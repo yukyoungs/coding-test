fe72142efc0cd17d428609b7fb281b08919a32e1
yukyoungs <syk9848@gmail.com>
2026-07-11 23:43:40 +0900

    Solve: Lv1 평균 구하기

--- Programmers/Lv1_평균_구하기/Solution.java
+++ Programmers/Lv1_평균_구하기/Solution.java
@@ -1,6 +1,16 @@
 class Solution {
     public double solution(int[] arr) {
         double answer = 0;
+        int j = arr.length;
+        int i = 0;
+        int sum = 0;
+
+        while (i < j) {
+            sum += arr[i];
+            i++;
+        }
+        answer = (double) sum / j;
+
         return answer;
     }
 }
