1e0b5cd2cfa8ba9d3cf1b602b8f8f9ddd1bd9c4a
yukyoungs <syk9848@gmail.com>
2026-09-02 21:59:58 +0900

    Solve: Lv1 하샤드 수

--- Programmers/Lv1_하샤드_수/Solution.java
+++ Programmers/Lv1_하샤드_수/Solution.java
@@ -1,6 +1,15 @@
 class Solution {
     public boolean solution(int x) {
         boolean answer = true;
+        int n = x;
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        answer = x % sum == 0;
+
         return answer;
     }
 }
