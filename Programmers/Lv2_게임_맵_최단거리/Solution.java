60631b6fcc2bde15fcc488ff0f56e3aee911533e
yukyoungs <syk9848@gmail.com>
2026-09-23 16:03:08 +0900

    Solve: Lv2 게임 맵 최단거리

--- Programmers/Lv2_게임_맵_최단거리/Solution.java
+++ Programmers/Lv2_게임_맵_최단거리/Solution.java
@@ -1,5 +1,4 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
@@ -8,33 +7,28 @@ class Solution {
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
 
-        boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0});
-        visited[0][0] = true;
+        maps[0][0] = 1;
 
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int x = cur[0];
             int y = cur[1];
 
-            if (x == n - 1 && y == m - 1) {
-                return maps[x][y];
-            }
+            for(int d=0; d<4; d++){
+                int nx = x + dx[d];
+                int ny = y + dy[d];
 
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(maps[nx][ny] != 1) continue;
 
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (visited[nx][ny] || maps[nx][ny] == 0) continue;
-
-                visited[nx][ny] = true;
-                maps[nx][ny] = maps[x][y] + 1;
+                maps[nx][ny] = maps[x][y]+1;
                 queue.offer(new int[]{nx, ny});
             }
         }
 
-        return -1;
+
+        return maps[n-1][m-1] == 1 ? -1 : maps[n-1][m-1];
     }
 }
